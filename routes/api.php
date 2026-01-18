e333512c166c7360178b002a603d9c9960f33c12
mfl4 <91680736+mfl4@users.noreply.github.com>
2026-01-19 02:26:22 +0700

    fix(route): change the route for better readibillity

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,11 @@
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/current-user', [AuthController::class, 'currentUser']);
+    Route::delete('/logout', [AuthController::class, 'logout']);
 });
