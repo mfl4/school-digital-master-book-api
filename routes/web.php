5c85fafc92b762c2c28bcdaa7e159b966f06964d
mfl4 <91680736+mfl4@users.noreply.github.com>
2026-01-19 21:51:04 +0700

    fix (routes): add fallback route to documentation

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/api/documentation');
 });
+
+Route::fallback(function () {
+    return redirect('/api/documentation');
+});
