bdd63108c310e47553bfee8455ca9f6012519d5d
mfl4 <91680736+mfl4@users.noreply.github.com>
2026-01-19 13:38:55 +0700

    feat(factory): add factory for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -14,7 +15,7 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    protected static ?string $password = null;
 
     /**
      * Define the model's default state.
@@ -28,6 +29,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => UserRole::ADMIN,
             'remember_token' => Str::random(10),
         ];
     }
@@ -39,6 +41,49 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Set user sebagai admin
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::ADMIN,
+        ]);
+    }
+
+    /**
+     * Set user sebagai guru dengan subject_id
+     */
+    public function guru(?int $subjectId = null): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::GURU,
+            'subject' => $subjectId,
+        ]);
+    }
+
+    /**
+     * Set user sebagai wali kelas dengan class
+     */
+    public function waliKelas(?string $class = null): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::WALI_KELAS,
+            'class' => $class ?? 'X-1',
+        ]);
+    }
+
+    /**
+     * Set user sebagai alumni dengan nim
+     */
+    public function alumni(?string $nim = null): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::ALUMNI,
+            'alumni' => $nim ?? fake()->numerify('##########'),
         ]);
     }
 }
